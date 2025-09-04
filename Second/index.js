6020bd23d0ffe516353434b0175257afa410aeb7
Karan Dhiman <dhimankaran260@gmail.com>
2025-09-04 22:26:54 +0530

    json parsing

--- Second/index.js
+++ Second/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const data = require('./MOCK_DATA.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/contacts',(req, res) => {
+  res.json(data)
 })
 
 app.listen(port, () => {
